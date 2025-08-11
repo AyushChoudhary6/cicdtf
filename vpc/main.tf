87d3980ed517c4f84e9ae7b672471c9fb0ae84ea
AyushChoudhary6 <ayushrjchoudhary2005@gmail.com>
2025-08-11 08:54:16 +0530

    Fix security group syntax and backend bucket name typo

--- vpc/main.tf
+++ vpc/main.tf
@@ -26,17 +26,23 @@ resource "aws_security_group" "sg" {
   vpc_id = aws_vpc.my_vpc.id
   name   = "my_sg"
 
-  ingress = {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH access"
   }
 
-  egress = {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "All outbound traffic"
+  }
+
+  tags = {
+    Name = "my_security_group"
   }
 }
