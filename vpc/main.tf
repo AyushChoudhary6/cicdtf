427be36d0ef82e805ae018e8ba326220f8430114
AYUSH CHOUDHARY <ayushrjchoudhary2005@gmail.com>
2025-08-11 03:20:08 +0000

    Edit main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -26,17 +26,21 @@ resource "aws_security_group" "sg" {
   vpc_id = aws_vpc.my_vpc.id
   name   = "my_sg"
 
-  ingress = {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  ingress = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
 
-  egress = {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  egress = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
 }
